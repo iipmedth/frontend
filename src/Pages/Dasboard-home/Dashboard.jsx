efccb1849072c2455bffcec4393081aa58b0cc36
Jeroen Ekkelkamp <jeroenekkelkamp@gmail.com>
2021-11-26 11:03:44 +0100

    Layout changes

--- src/Pages/Dasboard-home/Dashboard.jsx
+++ src/Pages/Dasboard-home/Dashboard.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import reactDom from "react-dom";
 import Navbar from "../../Components/Navbar/Navbar";
 
 export default class Dashboard extends React.Component {
   render() {
     return (
       <div className="row dashboard">
-        <div className="col-lg-2 box">
-          <Navbar />
-          
+        <div className="col-lg-1 box">
+          <Navbar />    
         </div>
         <div className="col-lg"> 
             <h1 className="box">hi</h1>
