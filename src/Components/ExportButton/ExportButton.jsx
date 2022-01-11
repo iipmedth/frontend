dc5689b4ed69dc9d1a57f3ee022865286472cd06
Jeroen Ekkelkamp <jeroenekkelkamp@gmail.com>
2022-01-11 15:32:27 +0100

    PDF export with custom font and patient name

--- src/Components/ExportButton/ExportButton.jsx
+++ src/Components/ExportButton/ExportButton.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import UploadFileIcon from "@mui/icons-material/UploadFile";
+import jsPDF from "jspdf";
+import { DataContext } from "../../context/DataContext";
+import "../../jspdf-font/Roboto-Regular";
 
 const ExportButton = () => {
+  const { selectedPatient } = useContext(DataContext);
+  const exportToPDF = () => {
+    let doc = new jsPDF("p", "pt");
+    doc.setFont("Roboto-Regular");
+    doc.setFillColor(0, 170, 167);
+    doc.rect(0, 0, 595, 30, "F");
+    doc.setTextColor(255, 255, 255);
+    doc.text(20, 20, "Hand profile of " + selectedPatient.name);
+    doc.save("Hand profile " + selectedPatient.name + ".pdf");
+  };
   return (
-    <button className="exportButton">
+    <button className="exportButton" onClick={() => exportToPDF()}>
       <UploadFileIcon sx={{ fontSize: 25, paddingRight: 1 }} />
       <span>Export to PDF</span>
     </button>
