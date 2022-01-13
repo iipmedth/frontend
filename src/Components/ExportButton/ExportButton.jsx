2fb7c78c5dfb142ecc57193e0f58c5d44b486201
Jeroen Ekkelkamp <jeroenekkelkamp@gmail.com>
2022-01-13 10:56:36 +0100

    info modal text change and pdf edit

--- src/Components/ExportButton/ExportButton.jsx
+++ src/Components/ExportButton/ExportButton.jsx
@@ -5,14 +5,27 @@ import { DataContext } from "../../context/DataContext";
 import "../../jspdf-font/Roboto-Regular";
 
 const ExportButton = () => {
-  const { selectedPatient } = useContext(DataContext);
+  const parseMil = (prefix, milimeterString) => {
+    let parsedMil =
+      prefix + ": " + String(parseFloat(milimeterString).toFixed(2)) + " mm";
+    return parsedMil;
+  };
+  const { selectedPatient, patientMeasurements, patientHandPercentiles } =
+    useContext(DataContext);
   const exportToPDF = () => {
+    console.log(patientMeasurements);
+    console.log(patientHandPercentiles);
     let doc = new jsPDF("p", "pt");
     doc.setFont("Roboto-Regular");
     doc.setFillColor(0, 170, 167);
     doc.rect(0, 0, 595, 30, "F");
     doc.setTextColor(255, 255, 255);
     doc.text(20, 20, "Hand profile of " + selectedPatient.name);
+
+    doc.setTextColor(0, 0, 0);
+    doc.setFontSize(9);
+    doc.text(20, 50, parseMil("Hand Length", patientMeasurements.hand_length));
+
     doc.save("Hand profile " + selectedPatient.name + ".pdf");
   };
   return (
